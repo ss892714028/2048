b611a06b5ecdc6dc9410c968a8c30756f59e8cca
ss892714028 <34635663+ss892714028@users.noreply.github.com>
2019-08-23 07:27:05 -0400

    monotonic reward

--- game/DQNagent.py
+++ game/DQNagent.py
@@ -11,7 +11,7 @@ import keras.backend as K
 class DQNAgent:
 
     def __init__(self, state_size=[1,4,4,1], epsilon = 0, gamma = 0.8, epsilon_decay = 0,
-                 epsilon_min = 0, learning_rate=0.0001, action_space=4, c=100):
+                 epsilon_min = 0, learning_rate=0.0001, action_space=4, c=200):
         self.epsilon = epsilon
         self.action = 0
         self.state_size = state_size
@@ -25,7 +25,6 @@ class DQNAgent:
         self.c = c
         self.memory = deque(maxlen=20000)
 
-
     def build(self):
         model = keras.models.Sequential()
 
@@ -33,32 +32,42 @@ class DQNAgent:
                               padding='valid',
                               activation='relu',
                               input_shape=[4,4,1],
-                              data_format='channels_last'))
-
+                              data_format='channels_last',
+                         kernel_initializer='random_uniform'))
         # Third convolutional layer
+        # model.add(Conv2D(256,[2,2], strides=1,
+        #                       padding='valid',
+        #                       activation='relu'))
         model.add(Conv2D(256,[2,2], strides=1,
                               padding='valid',
-                              activation='relu'))
-        model.add(Conv2D(256,[2,2], strides=1,
-                              padding='valid',
-                              activation='relu'))
+                              activation='relu',
+                         kernel_initializer='random_uniform'))
         # Flatten the convolution output
         model.add(Flatten())
-
         # First dense layer
-        model.add(Dense(512, activation='relu'))
-        model.add(Dense(256, activation='relu'))
+        model.add(Dense(512, activation='relu',
+                        kernel_initializer='random_uniform'))
+        model.add(Dense(256, activation='relu',
+                        kernel_initializer='random_uniform'))
         # Output layer
-        model.add(Dense(self.action_space))
+        model.add(Dense(self.action_space,
+                        kernel_initializer='random_uniform'))
 
         model.compile(loss=self.loss,
-                      optimizer=RMSprop(lr=self.learning_rate),
+                      optimizer=Adam(),
                       metrics=['accuracy'])
         print(model.summary())
         return model
 
     def store_memory(self, state, action, reward, next_state, game_over):
         self.memory.append((state, action, reward, next_state, game_over))
+
+    @staticmethod
+    def reset_weights(model):
+        session = K.get_session()
+        for layer in model.layers:
+            if hasattr(layer, 'kernel_initializer'):
+                layer.kernel.initializer.run(session=session)
 
     def act(self, state):
         if np.random.rand() <= self.epsilon:
@@ -68,11 +77,18 @@ class DQNAgent:
 
         return np.argmax(act_values[0])
 
-    def scale(self,d):
+    @staticmethod
+    def scale(d):
         d = d.flatten()
         d = [(np.math.log(i,2))/10 if i !=0 else 0 for i in d]
         d = np.array(d)
         return d
+
+    @staticmethod
+    def isMonotonic(A):
+
+        return (all(A[i] <= A[i + 1] for i in range(len(A) - 1)) or
+                all(A[i] >= A[i + 1] for i in range(len(A) - 1)))
 
     def experience_replay(self, batch_size):
         try:
@@ -88,7 +104,8 @@ class DQNAgent:
 
                 target = reward + self.gamma * np.amax(self.target.predict(next_state)[0])
             y = self.model.predict(state)[0]
-            y[action] =target
+
+            y[action] = target
             y = y.reshape(1,-1)
 
             hist = self.model.fit(state, y, epochs=1, verbose=0)
@@ -98,17 +115,38 @@ class DQNAgent:
             if self.epsilon > self.epsilon_min:
                 self.epsilon *= self.epsilon_decay
         print('avg training loss {}:'.format(np.array(loss).mean()))
-        print('training accuracy {}:'.format(np.array(accuracy).mean()))
+        # print('training accuracy {}:'.format(np.array(accuracy).mean()))
 
     def loss(self, y_true, y_pred):
         return K.square(y_pred[self.action] - y_true[self.action])
 
-    def soft_acc(self, y_true, y_pred):
-        return K.mean(K.equal(K.round(y_true), K.round(y_pred)))
-
     @staticmethod
-    def calculate_reward(next_state, joined_cells, empty, joinable):
-        result = np.where(next_state == np.max(next_state))
+    def minimax_scaler(x):
+        return (2*(x)/30) - 1
+
+    def calculate_reward(self, m, cells, empty, joinables):
+        # monotonic_row = []
+        # monotonic_col = []
+        # for i in m:
+        #     monotonic_row.append(self.isMonotonic(i))
+        # for i in m.T:
+        #     monotonic_col.append(self.isMonotonic(i))
+        # monotonic = 2 - max(monotonic_col.count(True), monotonic_row.count(True))
+
+
+
+        weight_matrix = np.array([[14, 12, 8, 6],
+                                  [12, 8, 6, 2],
+                                  [8, 6, 2, 1],
+                                  [6, 2, 1, 0]])
+        score = 0
+        for x in range(self.action_space):
+            for y in range(self.action_space):
+                score += weight_matrix[x][y] * m[x][y]
+
+        score = score/(np.mean(m)*20)
+
+        result = np.where(m == np.max(m))
         listOfCoordinates = np.array(list(zip(result[0], result[1])))
         temp = np.array([list(i) in [[0, 0], [0, 3], [3, 0], [3, 3]] for i in listOfCoordinates])
         if temp.any():
@@ -116,22 +154,22 @@ class DQNAgent:
         else:
             r_position = 0
 
-        if joined_cells == 0:
-            joined_cells = -1
+        if cells == 0:
+            cells = -1
         if empty == 0:
             empty = -5
-        if joined_cells == 0:
-            joined_cells = -2
-        if joinable == 0 and joined_cells <= 1:
-            joinable = -3
-        r = r_position + joined_cells + empty / 3 + max(joinable)
 
-        return r
+        if joinables == 0 and cells <= 1:
+            joinables = -3
+
+        return score + empty/2 + max(joinables) * 2
 
 
 if __name__ == "__main__":
     episodes = 100000
     agent = DQNAgent()
+    agent.reset_weights(agent.model)
+    agent.reset_weights(agent.target)
     # store all mean_score_cepochs
     s = []
     # average score of c epochs
@@ -173,7 +211,7 @@ if __name__ == "__main__":
             joinable = g.joinable
             joined_cells = g.joined_cells
             # calculate reward
-            reward = agent.calculate_reward(next_state,joined_cells,empty,joinable)
+            reward = agent.calculate_reward(g.board,joined_cells,empty,joinable)
             game_over = g.game_over
             # record reward
             total_r.append(reward)
